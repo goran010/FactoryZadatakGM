c6c40c68deb52554ccc35e9f045c155e329c1971
Goran Marković <91391493+goran010@users.noreply.github.com>
2022-07-13 22:02:12 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -3,27 +3,26 @@ const bottomRow = document.querySelector(".bottomRow");
 const plusBtn = document.querySelector(".plusBtn");
 const minusBtn = document.querySelector(".minusBtn");
 let direction;
-let widthLastTopPicture;
-let widthLastBottomPicture;
-
+let widthLastTopPicture=0;
+let widthLastBottomPicture=0;
 plusBtn.addEventListener("click", () => {
   if (widthLastTopPicture > 5 || widthLastTopPicture > 5) {
     widthLastTopPicture = topRow.lastElementChild.offsetWidth + 10;
     widthLastBottomPicture = bottomRow.lastElementChild.offsetWidth + 10;
-    }
-  else {
-      widthLastTopPicture = topRow.lastElementChild.offsetWidth +5;
-      widthLastBottomPicture = bottomRow.lastElementChild.offsetWidth + 5;
-    }
+  } else {
+    widthLastTopPicture = topRow.lastElementChild.offsetWidth + 5;
+    widthLastBottomPicture = bottomRow.lastElementChild.offsetWidth + 5;
+  }
   topRow.style.transform = "translate(" + widthLastTopPicture + "px)";
-  bottomRow.style.transform = "translate(" + widthLastBottomPicture + "px)";
+   bottomRow.style.transform = "translate(" + widthLastBottomPicture + "px)";
   direction = "right";
 });
 
 minusBtn.addEventListener("click", () => {
   widthLastTopPicture = topRow.firstElementChild.offsetWidth + 10;
-  //topRow.style.transform = "translate(-" + widthLastTopPicture + "px)";
-  topRow.style.transform = "translate(0)";
+  widthLastBottomPicture = bottomRow.firstElementChild.offsetWidth + 10;
+  topRow.style.transform = "translate(-" + widthLastTopPicture + "px)";
+  bottomRow.style.transform = "translate(-" + widthLastBottomPicture + "px)";
   direction = "left";
 });
 
@@ -45,10 +44,19 @@ topRow.addEventListener("transitionend", () => {
   }
 });
 bottomRow.addEventListener("transitionend", () => {
-  bottomRow.prepend(bottomRow.lastElementChild);
-  bottomRow.style.transition = "none";
-  bottomRow.style.transform = "translate(0)";
-  setTimeout(() => {
-    bottomRow.style.transition = "all 0.5s";
-  });
+  if (direction === "right") {
+    bottomRow.prepend(bottomRow.lastElementChild);
+    bottomRow.style.transition = "none";
+    bottomRow.style.transform = "translate(0)";
+    setTimeout(() => {
+      bottomRow.style.transition = "all 0.5s";
+    });
+  } else {
+    bottomRow.append(bottomRow.firstElementChild);
+    bottomRow.style.transition = "none";
+    bottomRow.style.transform = "translate(0)";
+    setTimeout(() => {
+      bottomRow.style.transition = "all 0.5s";
+    });
+  }
 });
---
+++ assets.js
@@ -0,0 +1,25 @@
+document.querySelector(".bottomRowContainer").innerHTML = `
+    <div class="bottomRow">
+        <img src="Assets/slider-image-4.jpg" alt="" />
+        <img src="Assets/slider-image-1.jpg" alt="" />
+        <img src="Assets/slider-image-2.jpg" alt="" />
+        <img src="Assets/slider-image-3.jpg" alt="" />
+    </div>
+    `;
+document.querySelector(".topRowContainer").innerHTML = `
+    <div class="topRow">
+        <img src="Assets/slider-image-9.jpg" alt="" />
+        <img src="Assets/slider-image-8.jpg" alt="" />
+        <img src="Assets/slider-image-5.jpg" alt="" />
+        <img src="Assets/slider-image-4.jpg" alt="" />
+        <img src="Assets/slider-image-7.jpg" alt="" />
+        <img src="Assets/slider-image-6.jpg" alt="" />
+    </div>`;
+document.querySelector(".sideBar").innerHTML = `
+    <h1>Lorem ipsum dolor sit amet consectetuer adipiscing elit</h1>
+    <div>
+        Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper
+        suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis
+        autem vel eum iriure dolor in hendrerit in vulputate velit esse
+        molestie consequat.
+    </div>`;
