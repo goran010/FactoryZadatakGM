e45d572b287850d9f8d5f9e4c68d70860ce93e9f
Goran Marković <91391493+goran010@users.noreply.github.com>
2022-07-20 17:37:33 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
-const topRow = document.querySelector(".topRow");
-const bottomRow = document.querySelector(".bottomRow");
-const plusBtn = document.querySelector(".plusBtn");
-const minusBtn = document.querySelector(".minusBtn");
-let direction;
+const topRow = $(".topRow")[0];
+const bottomRow = $(".bottomRow")[0];
+const plusBtn = $(".plusBtn")[0];
+const minusBtn = $(".minusBtn")[0];
+const body = $("body")[0];
+let directionToRight;
 
+//functions
 const addAnimation = (element) => {
-  element.style.transition = "ease transform 0.5s";
+  setTimeout(() => {
+    element.style.transition = "ease transform 0.5s";
+  });
 };
+
 const removeAnimation = (element) => {
   element.style.transition = "none";
 };
@@ -19,22 +24,29 @@ const removeRightMargin = (image) => {
 };
 
 //adding margin that neutralizes the difference in image width
+
 createRightMargin(topRow.lastElementChild);
 createRightMargin(bottomRow.lastElementChild);
 
-//adding animation
-setTimeout(() => {
-  addAnimation(bottomRow);
-  addAnimation(topRow);
-});
+
+topRow.style.transform = "translate(300px)";
+bottomRow.style.transform = "translate(300px)";
+
+
+  setTimeout(() => {
+    addAnimation(bottomRow);
+    addAnimation(topRow);
+  });
 
 //adding event listener on right button
-$(".plusBtn").click(() => {
+$(".plusBtn").on("click", () => {
   //translating top row of pictures to right
+  
   topRowImages = $(".topRow>img");
   topRow.style.transform = `translate(${
     315 + topRowImages[topRowImages.length - 2].offsetWidth
-  }px)`;
+    }px)`;
+   
   //translating bottom row of pictures to right
   bottomRowImages = $(".bottomRow>img");
   bottomRow.style.transform =
@@ -42,12 +54,13 @@ $(".plusBtn").click(() => {
     (315 + bottomRowImages[bottomRowImages.length - 2].offsetWidth) +
     "px)";
   //defining the direction of image movement
-  direction = "right";
+  directionToRight = true;
 });
 
 //adding event listener on left button
-$(".minusBtn").click(() => {
+$(".minusBtn").on("click", () => {
   //translating top row of pictures to left
+
   topRowImages = $(".topRow>img");
   topRow.style.transform = `translate(${
     300 - topRowImages[topRowImages.length - 1].offsetWidth
@@ -58,22 +71,20 @@ $(".minusBtn").click(() => {
     300 - bottomRowImages[bottomRowImages.length - 1].offsetWidth
   }px)`;
   //defining the direction of image movement
-  direction = "left";
+  directionToRight = false;
 });
 
 //top row of pictures, adding listener after tranasition
 topRow.addEventListener("transitionend", () => {
   //case when direction of moving pictures is to right
-  if (direction == "right") {
+  if (directionToRight) {
     topRowImages = $(".topRow>img");
     removeRightMargin(topRowImages[topRowImages.length - 1]);
     createRightMargin(topRowImages[topRowImages.length - 2]);
     removeAnimation(topRow);
     topRow.prepend(topRow.lastElementChild);
     topRow.style.transform = "translate(305px)";
-    setTimeout(() => {
-      addAnimation(topRow);
-    });
+    addAnimation(topRow);
   }
   //case when direction of moving pictures is to left
   else {
@@ -84,25 +95,21 @@ topRow.addEventListener("transitionend", () => {
     removeAnimation(topRow);
     topRow.append(topRow.firstElementChild);
     topRow.style.transform = "translate(310px)";
-    setTimeout(() => {
-      addAnimation(topRow);
-    });
+    addAnimation(topRow);
   }
 });
 
 //bottom row of pictures, adding listener after tranaslate
 bottomRow.addEventListener("transitionend", () => {
   //case when direction of moving pictures is right
-  if (direction == "right") {
+  if (directionToRight) {
     bottomRowImages = $(".bottomRow>img");
     removeRightMargin(bottomRowImages[bottomRowImages.length - 1]);
     createRightMargin(bottomRowImages[bottomRowImages.length - 2]);
     removeAnimation(bottomRow);
     bottomRow.prepend(bottomRow.lastElementChild);
     bottomRow.style.transform = "translate(305px)";
-    setTimeout(() => {
-      addAnimation(bottomRow);
-    });
+    addAnimation(bottomRow);
   }
   //case when direction of moving pictures is left
   else {
@@ -113,24 +120,6 @@ bottomRow.addEventListener("transitionend", () => {
     removeAnimation(bottomRow);
     bottomRow.append(bottomRow.firstElementChild);
     bottomRow.style.transform = "translate(310px)";
-    setTimeout(() => {
-      addAnimation(bottomRow);
-    });
+    addAnimation(bottomRow);
   }
-});
-
-//changing color of arrow pointing right
-$(".plusBtn").mouseenter(() => {
-  document.querySelector(".rightArrow").src = "Assets/arrow-gray-left.png";
-});
-$(".plusBtn").mouseleave(() => {
-  document.querySelector(".rightArrow").src = "Assets/arrow-blue-left.png";
-});
-
-//changing color of arrow pointing left
-$(".minusBtn").mouseenter(() => {
-  document.querySelector(".leftArrow").src = "Assets/arrow-gray-right.png";
-});
-$(".minusBtn").mouseleave(() => {
-  document.querySelector(".leftArrow").src = "Assets/arrow-blue-right.png";
 });
